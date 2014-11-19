884e71157d7909baf40b62b162185cd8299d9367
George Heeres <gheeres@gmail.com>
2014-11-19 07:26:59 -0600

    Refactoring. More experimentation. Creation of "ball" dynamically based on touch position.

--- src/com/heeresonline/justhoops/PhysicsWorld.java
+++ src/com/heeresonline/justhoops/PhysicsWorld.java
@@ -1,6 +1,7 @@
 package com.heeresonline.justhoops;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.jbox2d.collision.AABB;
@@ -13,53 +14,116 @@ import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 
+import android.util.Log;
+
 public class PhysicsWorld {
-  private final static int    WORLD_ENTITY = 0x01;
-  private final static int    PHYSICS_ENTITY = 0x01 << 1;
+  private final static String TAG = "PhysicsWorld";
+  
+  private final static int WORLD_ENTITY = 0x01;
+  private final static int PHYSICS_ENTITY = 0x01 << 1;
+  
+//  private Vec2 upperLeft;
+//  private Vec2 lowerRight;
+//  private Vec2 upperRight;
+//  private Vec2 lowerLeft;
   
   private List<Body> bodies;
-  private AABB worldAABB;
+  //private AABB worldAABB;
   private World world;
-  private BodyDef groundBodyDef;
-  //private PolygonDef boxShapeDef;
+  private Body ground;
+  //private Body box;
   
-  public PhysicsWorld(int width, int height) {
-
-    create(width, height);  
+  private float minX;
+  private float minY;
+  private float maxX;
+  private float maxY;
+  
+  public PhysicsWorld(float minX, float minY, float maxX, float maxY) {
+    Log.d(TAG, String.format("Bounds: X(%5.2f / %5.2f), Y(%5.2f / %5.2f)",
+                             minX, maxX, minY, maxX));
+ 
+    this.minX = minX;
+    this.maxX = maxX;
+    this.minY = minY;
+    this.maxY = maxY;
+    
+    create();
+  }
+ 
+  private void setBoundingBox(Vec2 upperLeft, Vec2 lowerRight) {
+//    Vec2 upperRight = new Vec2(lowerRight.x, upperLeft.y);
+//    Vec2 lowerLeft = new Vec2(upperLeft.x, lowerRight.y);
+//    
+//    // Set up walls body definitions
+//    BodyDef leftDef = new BodyDef();
+//    leftDef.position.set(lowerLeft);
+//    BodyDef rightDef = new BodyDef();
+//    rightDef.position.set(lowerRight);
+//    BodyDef topDef = new BodyDef();
+//    topDef.position.set(upperLeft);
+//    BodyDef bottomDef = new BodyDef();
+//    bottomDef.position.set(lowerLeft);
+//
+//    EdgeShape topEdge = new EdgeShape();
+//    topEdge.set(upperLeft, upperRight);
+//    EdgeShape bottomEdge = new EdgeShape();
+//    bottomEdge.set(lowerLeft, lowerRight);
+//    EdgeShape leftEdge = new EdgeShape();
+//    leftEdge.set(upperLeft, lowerLeft);
+//    EdgeShape rightEdge = new EdgeShape();
+//    rightEdge.set(upperRight, lowerRight);
+//
+//    box = world.createBody(bottomDef);
+//    box.createFixture(topEdge, 0.0f);
+//    box.createFixture(bottomEdge, 0.0f);
+//    box.createFixture(leftEdge, 0.0f);
+//    box.createFixture(rightEdge, 0.0f);
   }
   
-  public Iterable<Body> getBodies() {
-    return(bodies);
+  public void create() 
+  {
+    create(new Vec2(0.0f, -9.8f));
   }
   
-  public void create(int width, int height) 
+  public void create(Vec2 gravity) 
   {
     bodies = new ArrayList<Body>();
 
-    world = new World(new Vec2(0.0f, (float)10.0f));
+    world = new World(gravity);
     world.setAllowSleep(true);
+  
+//    setBoundingBox(upperLeft, lowerRight);
 
-    // The Ground
-    groundBodyDef = new BodyDef();
-    groundBodyDef.position.set(new Vec2(0.0f, -10.0f));
-    Body groundBody = world.createBody(groundBodyDef);
-    PolygonShape groundBox = new PolygonShape();
-    groundBox.setAsBox(50.0f, 10.0f);
-    groundBody.createFixture(groundBox, 0.0f);
+//    // The Ground
+//    BodyDef groundBodyDef = new BodyDef();
+//    groundBodyDef.position.set(new Vec2(0.0f, -10.0f));
+//    ground = world.createBody(groundBodyDef);
+//    PolygonShape groundBox = new PolygonShape();
+//    groundBox.setAsBox(50.0f, 10.0f);
+//    ground.createFixture(groundBox, 0.0f);
+
+    BodyDef groundDef = new BodyDef();
+    groundDef.position.set(0.0f, 0.0f);
+    EdgeShape groundShape = new EdgeShape();
+    groundShape.set(new Vec2(minX, minY), new Vec2(maxX, minY));
+    ground = world.createBody(groundDef);
+    ground.createFixture(groundShape, 0.0f);
     
-    // Our (Square) Ball 
-    BodyDef ballDef = new BodyDef();
-    ballDef.type = BodyType.DYNAMIC;
-    ballDef.position.set(4.0f, 4.0f);
-    Body ballBody = world.createBody(ballDef);
-    PolygonShape ballBox = new PolygonShape();
-    ballBox.setAsBox(1.0f, 1.0f);
-    FixtureDef ballFixtureDef = new FixtureDef();
-    ballFixtureDef.shape = ballBox;
-    ballFixtureDef.density = 1.0f;
-    ballFixtureDef.friction = 0.3f;
-    ballBody.createFixture(ballFixtureDef);
-    bodies.add(ballBody);
+
+//    // Our (Square) Ball 
+//    BodyDef ballDef = new BodyDef();
+//    ballDef.type = BodyType.DYNAMIC;
+//    ballDef.position.set(getCenterX(), getCenterY());
+//    Body ballBody = world.createBody(ballDef);
+//    PolygonShape ballBox = new PolygonShape();
+//    ballBox.setAsBox(1.0f, 1.0f);
+//    FixtureDef ballFixtureDef = new FixtureDef();
+//    ballFixtureDef.shape = ballBox;
+//    ballFixtureDef.density = 1.0f;
+//    ballFixtureDef.friction = 0.3f;
+//    ballFixtureDef.restitution = 0.8f;
+//    ballBody.createFixture(ballFixtureDef);
+//    bodies.add(ballBody);
     
     //Body groundBody = world.createDynamicBody(groundBodyDef);
     //boxShapeDef = new PolygonDef();
@@ -106,7 +170,71 @@ public class PhysicsWorld {
 //    edgeScreen.createFixture(floorFixture);
   }
   
+  public void addBox(Vec2 pos, Vec2 size) {
+    addBox(pos.x, pos.y, size.x, size.y);
+  }
+  public void addBox(Vec2 pos, float width, float height) {
+    addBox(pos.x, pos.y, width, height);
+  }
+  public void addBox(float x, float y, float width, float height) {
+    // Our (Square) Ball 
+    BodyDef ballDef = new BodyDef();
+    ballDef.type = BodyType.DYNAMIC;
+    ballDef.position.set(x, y);
+    Body ballBody = world.createBody(ballDef);
+    PolygonShape ballBox = new PolygonShape();
+    ballBox.setAsBox(width, height);
+    FixtureDef ballFixtureDef = new FixtureDef();
+    ballFixtureDef.shape = ballBox;
+    ballFixtureDef.density = 1.0f;
+    ballFixtureDef.friction = 0.3f;
+    ballFixtureDef.restitution = 0.8f;
+    ballBody.createFixture(ballFixtureDef);
+    synchronized(bodies) {
+      bodies.add(ballBody);
+    }
+  }
+  
   public void step(float deltaTime) {
     world.step(1f/60f, 6, 2);
+
+    int index = 0;
+    Iterable<Body> bodies = getBodies();
+    synchronized(bodies) {
+      for(Body body : getBodies()) {
+        Vec2 position = body.getPosition();
+        float angle = body.getAngle();
+        Log.d(TAG, String.format("[%d] x: %4.2f, y: %4.2f, angle: %4.2f\n", 
+                                 index++, position.x, position.y, angle));
+      }
+    }
+  }
+
+  public void dispose() {
+    world.destroyBody(ground);
+    //world.destroyBody(box);
+    Iterable<Body> bodies = getBodies();
+    synchronized(bodies) {
+      for(Body body: getBodies()) {
+        world.destroyBody(body);
+      }
+    }
+  }
+  
+  public Vec2 getCenter() {
+    return(new Vec2(getCenterX(), getCenterY()));
+  }
+
+  public float getCenterX() {
+    return(minX + (maxX - minX) / 2);
+  }
+
+  public float getCenterY() {
+    return(minY + (maxY - minY) / 2);
+  }
+
+  public Iterable<Body> getBodies() {
+    if (bodies == null) bodies = new ArrayList<Body>();
+    return(bodies);
   }
 }
--- src/com/heeresonline/justhoops/RenderView.java
+++ src/com/heeresonline/justhoops/RenderView.java
@@ -1,5 +1,8 @@
 package com.heeresonline.justhoops;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.jbox2d.collision.shapes.EdgeShape;
 import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
@@ -16,7 +19,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.Paint.Align;
 import android.graphics.Rect;
 import android.util.Log;
@@ -26,7 +31,9 @@ import android.view.SurfaceView;
 
 public class RenderView extends SurfaceView implements Runnable {
   private static final String TAG = "RenderView";
-  private static final float PIXELS_TO_METERS = 100f;
+  private static final float WORLD_WIDTH = 10.0f;
+  private static final float WORLD_HEIGHT = 10.0f;
+  
   private final static int MAX_FPS = 50; //desired fps   
   private final static int MAX_FRAME_SKIPS = 5; // maximum number of frames to be skipped    
   private final static int FRAME_PERIOD = 1000 / MAX_FPS; // the frame period  
@@ -37,10 +44,15 @@ public class RenderView extends SurfaceView implements Runnable {
   private volatile boolean isRunning = false;
   
   private PhysicsWorld world;
+  private Camera camera;
+  private float height;
+  private float width;
   private Bitmap bitmap;
   
   private Paint paint;
   private Rect bounds;
+  
+  private float time;
   
   public RenderView(Context context) {
     super(context);
@@ -64,12 +76,15 @@ public class RenderView extends SurfaceView implements Runnable {
       int framesSkipped = 0;
       float deltaTime = (System.nanoTime() - startTime) / 1000000000.0f;
       startTime = System.nanoTime();
-      Log.d(TAG, String.format("DeltaTime: %f", deltaTime));
+      //Log.d(TAG, String.format("DeltaTime: %f", deltaTime));
 
       Canvas canvas = holder.lockCanvas();
-      if (world == null) world = new PhysicsWorld(canvas.getWidth(), canvas.getHeight());
+      width = canvas.getWidth();
+      height = canvas.getHeight();
+
       update(deltaTime);
       render(canvas, deltaTime);
+      
       holder.unlockCanvasAndPost(canvas);
       
       // Constant frame rate / speed
@@ -82,8 +97,8 @@ public class RenderView extends SurfaceView implements Runnable {
         }
       }
       while ((sleepTime < 0) && (framesSkipped < MAX_FRAME_SKIPS)) {
-        Log.d(TAG, String.format("%d / %d frame(s) skipped. Behind by %d msec", 
-                                 framesSkipped+1, MAX_FRAME_SKIPS, sleepTime));
+        //Log.d(TAG, String.format("%d / %d frame(s) skipped. Behind by %d msec", 
+        //                         framesSkipped+1, MAX_FRAME_SKIPS, sleepTime));
         update(sleepTime);
         sleepTime += FRAME_PERIOD;
         framesSkipped++;
@@ -102,11 +117,17 @@ public class RenderView extends SurfaceView implements Runnable {
     int height = canvas.getHeight();
     int width = canvas.getWidth();
     
-    for(Body body : world.getBodies()) {
-      Bitmap sprite = bitmap; //(Bitmap) body.getUserData();
-      canvas.drawBitmap(sprite, 
-          body.getPosition().x * PIXELS_TO_METERS - (sprite.getWidth() / 2), 
-          body.getPosition().y * PIXELS_TO_METERS - (sprite.getHeight() / 2), paint);
+    Iterable<Body> bodies = getWorld().getBodies();
+    synchronized(bodies) {
+      for(Body body : bodies) {
+        Bitmap sprite = bitmap; //(Bitmap) body.getUserData();
+        Vec2 position = camera.worldToScreen(body.getPosition());
+        Log.d(TAG, String.format("x: %5.5f, y: %5.4f, angle: %5.4f", 
+                                   position.x, height - position.y, body.getAngle() * (180/(float) Math.PI)));
+        //Matrix matrix = new Matrix();
+        //matrix.setRotate(body.getAngle() * (180/(float) Math.PI));
+        canvas.drawBitmap(sprite, position.x, height - position.y, paint);
+      }
     }
     
     paint.setColor(Color.WHITE);
@@ -114,9 +135,21 @@ public class RenderView extends SurfaceView implements Runnable {
     
     paint.setTextAlign(Align.RIGHT);
     paint.setTextSize(24);
-    String statistics = String.format("%3.6f", deltaTime);
+    time = ((time == 0) ? 1 : time) * 0.9f + deltaTime * 0.1f; 
+    String statistics = String.format("fps: %5.2f [delta: %3.6f]", 1/time, deltaTime);
     paint.getTextBounds(statistics, 0, statistics.length()-1, bounds);
     canvas.drawText(statistics, width, height - bounds.height(), paint);
+    
+    paint.setTextAlign(Align.LEFT);
+    Iterator<Body> iterator = bodies.iterator();
+    if (iterator.hasNext()) {
+      Body body = iterator.next();
+      if (body != null) {
+        Vec2 position = camera.worldToScreen(body.getPosition());
+        canvas.drawText(String.format("pos: %5.1f,%5.1f (%5.3f)", position.x, position.y, body.getAngle() * (180/(float) Math.PI)), 
+                        0, height - bounds.height(), paint);
+      }
+    }
   }
   
   public void resume() {
@@ -136,10 +169,44 @@ public class RenderView extends SurfaceView implements Runnable {
     }
   }
   
+  public PhysicsWorld getWorld() {
+    if (world == null) world = new PhysicsWorld(0, 0, WORLD_WIDTH, WORLD_HEIGHT);
+    return(world);
+  }
+
+  public Camera getCamera() {
+    if (camera == null) camera = new Camera(new Vec2(WORLD_WIDTH, WORLD_HEIGHT), new Vec2(width, height));
+    return(camera);
+  }
+
   @Override
   public boolean onTouchEvent(MotionEvent event) {
-    if (world != null) {
-      world = null;
+    PhysicsWorld world = getWorld();
+    
+    int action = event.getAction() & MotionEvent.ACTION_MASK;
+    int pointerIndex = (event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+    int pointerId = event.getPointerId(pointerIndex);
+    switch(action) {
+      case MotionEvent.ACTION_DOWN:
+      case MotionEvent.ACTION_POINTER_DOWN:
+        world.addBox(getCamera().screenToWorld(event.getX(pointerIndex), height - event.getY(pointerIndex)), 
+                                               getCamera().screenToWorld(bitmap.getWidth(), bitmap.getHeight()));
+        //points[pointerId] = new PointF(event.getX(pointerIndex), event.getY(pointerIndex));
+        break;
+      case MotionEvent.ACTION_CANCEL:
+      case MotionEvent.ACTION_UP:
+      case MotionEvent.ACTION_POINTER_UP:
+        break;
+      case MotionEvent.ACTION_MOVE:
+        //for(int index = 0, events = event.getPointerCount(); index < events; index++) {
+        //  pointerId = event.getPointerId(index);
+        //  points[pointerId].x = event.getX(index);
+        //  points[pointerId].y = event.getY(index);
+        //}
+        break;
+      
+      default:
+        return(false);
     }
     return super.onTouchEvent(event);
   }
