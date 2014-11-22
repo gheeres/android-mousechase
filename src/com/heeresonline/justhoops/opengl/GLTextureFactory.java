553fa7f06ced22ea786033d797bda8de098253b6
George Heeres <gheeres@gmail.com>
2014-11-22 13:33:11 -0600

    Start of GLTextureFactory class.

---
+++ src/com/heeresonline/justhoops/opengl/GLTextureFactory.java
@@ -0,0 +1,31 @@
+package com.heeresonline.justhoops.opengl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.res.Resources;
+import android.opengl.GLES20;
+import android.util.Log;
+
+public class GLTextureFactory {
+  public static final String TAG = "GLTextureFactory";
+  //private static int[] internalTexture = new
+  //public static final Map<String, Integer> shaders = new HashMap<String, Integer>();
+  public static final Map<String, Integer> textures = new HashMap<String, Integer>();
+
+//  /**
+//   * Add the specified texture.
+//   * @param name The name of the texture.
+//   * @param texture The texture itself.
+//   * @return The id of the Texture program.
+//   */
+//  public static int addTexture(String name, Bitmap texture) {
+//    Log.d(TAG, String.format("Created shader program (%d) as %s.", program, name));
+//    return(program);
+//  }
+//  
+}
